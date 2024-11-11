3ec3c9dbc1077c2e3d658d61cd01e4a60213abdd
Owen <owendonnelly325@gmail.com>
2024-11-11 18:38:55 -0500

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,1 +1,7 @@
-
+public class Admin extends User {
+	public Admin(String username, String rawPass, String firstName, String lastName) {
+		super(username, rawPass, firstName, lastName);
+	}
+	
+	
+}
