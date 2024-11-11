14a196ef34f2437cfc4613f149d140c2642675b8
Owen <owendonnelly325@gmail.com>
2024-11-11 15:06:31 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,19 @@
-
+public class User {
+	private String username;
+	private String encPass;
+	private String firstName;
+	private String lastName;
+	
+	public User(String username, String rawPass, String firstName, String lastName) {
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		encPass = Hash.sha256(rawPass);
+		
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+}
