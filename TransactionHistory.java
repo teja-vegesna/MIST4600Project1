572cf54ff6e596203c1b5a0c9acf5c11d519609d
Owen <owendonnelly325@gmail.com>
2024-11-11 16:02:16 -0500

    Update TransactionHistory.java

--- TransactionHistory.java
+++ TransactionHistory.java
@@ -1,1 +1,26 @@
+import java.util.*;
 
+public class TransactionHistory {
+	private ArrayList<Transaction> transactions;
+	
+    public TransactionHistory() {
+        this.transactions = new ArrayList<>();
+    }
+	
+	public void addTransaction(Transaction transaction) {
+		transactions.add(transaction);
+	}
+	
+	public ArrayList<Transaction> getTransactionsForUser(User user) {
+		ArrayList<Transaction> userTransactions = new ArrayList<>();
+		for (Transaction transaction : transactions) {
+            if (transaction.getUser().equals(user) || 
+                (transaction.getRecipient() != null && transaction.getRecipient().equals(user))) {
+                userTransactions.add(transaction);
+            }
+        }
+		
+		return userTransactions;
+	}
+	
+}
