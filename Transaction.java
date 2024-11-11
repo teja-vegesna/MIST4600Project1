fd1fb0ba6611aca690686878edc698e6f3bd7b84
Owen <owendonnelly325@gmail.com>
2024-11-11 16:02:49 -0500

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -5,27 +5,43 @@ public class Transaction {
     private double amount;
     private Date date;
     private User user;
+    private User recipient;
 
     public Transaction(String type, double amount, User user) {
         this.type = type;
         this.amount = amount;
         this.date = new Date(); // Sets the current date and time
         this.user = user;
+        this.recipient = recipient;
     }
 
     public String toString() {
-        String pastTensed;
-        if ("deposit".equals(type)) {
-            pastTensed = "deposited";
-        } else if ("withdrawal".equals(type)) {
-            pastTensed = "withdrew";
-        } else {
-            pastTensed = "performed";
-        }
+        String pastTensed = switch (type) {
+            case "deposit" -> "deposited";
+            case "withdrawal" -> "withdrew";
+            case "transfer" -> "transferred to " + (recipient != null ? recipient.getUsername() : "unknown");
+            default -> "performed";
+        };
         return user.getUsername() + " " + pastTensed + " " + amount + " on " + date;
     }
     
-    public getType() {
-    	
+    public String getType() {
+    	return type;
+    }
+    
+    public double getAmount() {
+    	return amount;
+    }
+    
+    public Date getDate() {
+    	return date;
+    }
+    
+    public User getUser() {
+    	return user;
+    }
+    
+    public User getRecipient() {
+    	return recipient;
     }
 }
