f1c80000bd7dc91fcd563338a38857388187593d
Owen <owendonnelly325@gmail.com>
2024-11-11 19:09:21 -0500

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,7 +1,14 @@
+import java.util.*;
+
 public class Admin extends User {
+	private AccountManager accountManager;
+	
 	public Admin(String username, String rawPass, String firstName, String lastName) {
 		super(username, rawPass, firstName, lastName);
 	}
 	
+	public ArrayList<User> getUsers() {
+		return new ArrayList<>(accountManager.users.values());
+    }
 	
 }
